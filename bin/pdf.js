de6ef65b433b7cb4997926483435b505a6ff5119
Nick Hoffman <njhoffman1982@gmail.com>
2022-11-15 22:04:11 -0600

    updates

--- bin/pdf.js
+++ bin/pdf.js
@@ -5,7 +5,7 @@ const path = require('path');
 const pdf = require('html-pdf');
 const options = { format: 'Letter' };
 
-const filePath = path.join(__dirname, '../build/bundle/index.html');
+const filePath = path.join(__dirname, '../build/index.html');
 const outputPath = path.join(__dirname, '../resume.pdf');
 
 if (!fs.existsSync(filePath)) {
@@ -14,7 +14,7 @@ if (!fs.existsSync(filePath)) {
 
 const html = fs.readFileSync(filePath, 'utf8');
 
-console.log('HTML', html);
+// console.log('HTML', html);
 pdf.create(html, options).toFile(outputPath, (err, res) => {
   if (err) return console.log(err);
   console.log(res); // { filename: '/app/businesscard.pdf' }
